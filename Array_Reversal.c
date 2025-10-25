2ed51881574763858d95aad3589254ce910160b7
Maham Aurangzaib <mahamzaib02@gmail.com>
2025-10-25 18:29:11 +0500

    Update Array_Reversal.c

--- Array_Reversal.c
+++ Array_Reversal.c
@@ -8,18 +8,15 @@ int main()
 
     arr = (int*) malloc(num * sizeof(int));
 
-    // Reading array elements
     for(int i = 0; i < num; i++) {
         scanf("%d", arr + i);
     }
-
-    // Print array in reverse order
     for(int i = num - 1; i >= 0; i--) {
         printf("%d", *(arr + i));  // same as arr[i]
         if(i != 0) printf(" ");    // avoid trailing space
     }
 
     printf("\n");
-    free(arr); // free allocated memory
+    free(arr); 
     return 0;
 }
